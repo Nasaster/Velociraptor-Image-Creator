651e3ea60370b1bce8d09edbb93b72b62331e4b4
thanasis tsokaktsidis <nas.tsokaktsidis@gmail.com>
2015-07-02 10:59:25 +0200

    application is now stable ^_^

--- source/js/controllers/imagePropertiesController.js
+++ source/js/controllers/imagePropertiesController.js
@@ -1,7 +1,7 @@
 app.controller('ImagePropertiesController', ['$scope', function($scope) {
   $scope.scores = [
   {
-  	"type": "String",
+  	"type": "String",		// transparent toggle (e.g. PNG for transparent, JPG for non-transparent)
   	"grid": "Boolean",
   	"gridProperties": {
   		"width": "Number",
