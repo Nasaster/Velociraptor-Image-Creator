649f4fd3a5ecfc366192d8a65c8d5d3cbc5e30db
thanasis tsokaktsidis <nas.tsokaktsidis@gmail.com>
2015-07-08 18:14:06 +0200

    fix the ng-models

--- source/js/controllers/VelociraptorFormController.js
+++ source/js/controllers/VelociraptorFormController.js
@@ -1,6 +1,19 @@
 app.controller('VelociraptorForm', ['$scope', function($scope)
 {
-	// Velociraptor form goes here
+	// the model specifiying what the data looks like
 
-	console.log( "VelociraptorForm is active!!" );
+	this.model = {
+		width: 10,
+		height: 10,
+		unit: 'px',
+		transparent: false,
+		hex: [ '#FF0000', '#000000', '#FFFFFFF' ]
+	};
+
+	// function invoked when the form is submitted
+
+	this.submit = function( form )
+	{
+		console.log( this.model );
+	};
 }]);
--- source/js/controllers/imagePropertiesController.js
+++ source/js/controllers/imagePropertiesController.js
@@ -1,48 +1,12 @@
-// app.controller('ImagePropertiesController', ['$scope', function($scope) {
-	
-// 	$scope.title = 'Velociraptor the real Image Creator';
- 
-
-
-
-
-  // $scope.apps = [
-  // {
-  // 	"type": "String",		// transparent toggle (e.g. PNG for transparent, JPG for non-transparent)
-  // 	"grid": "Boolean",
-  // 	"gridProperties": {
-  // 		"width": "Number",
-  // 		"height": "Number"
-  // 	}
-  // }];
-//}]);
-
-
 app.controller('ImagePropertiesController', ['$scope', function($scope) {
-  $scope.apps = [ 
-	  { 
-	    icon: 'img/move.jpg', 
-	    title: 'MOVE', 
-	    developer: 'MOVE, Inc.', 
-	    price: 0.99 
-	  }, 
-	  { 
-	    icon: 'img/shutterbugg.jpg', 
-	    title: 'Shutterbugg', 
-	    developer: 'Chico Dusty', 
-	    price: 2.99 
-	  },
-	  {
-	    icon: 'img/gameboard.jpg',
-	    title: 'Gameboard',
-	    developer: 'Armando P.',
-	    price: 1.99
-	  },
-	  {
-	    icon: 'img/forecast.jpg',
-	    title: 'Forecast',
-	    developer: 'Forecast',
-	    price: 1.99
-	  }
-	];
+    $scope.apps = [{
+        "config": {
+            "type": "String",
+            "grid": "Boolean",
+            "gridProperties": {
+                "width": "Number",
+                "height": "Number"
+            }
+        }
+    }];
 }]);
