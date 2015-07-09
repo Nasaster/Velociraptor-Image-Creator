037e9170221a70d762f8102338d53620ad4456bf
thanasis tsokaktsidis <nas.tsokaktsidis@gmail.com>
2015-07-09 17:09:55 +0200

    fixing the property names and addind a canvas directive

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,1 @@
 var app = angular.module( "Velociraptor", [ "templates" ]);
-
-// controllers defined in js/controllers
-// directives for HTML templates defined in js/directives
--- source/js/controllers/VelociraptorFormController.js
+++ source/js/controllers/VelociraptorFormController.js
@@ -6,8 +6,21 @@ app.controller('VelociraptorForm', ['$scope', function($scope)
 		width: 10,
 		height: 10,
 		unit: 'px',
+
 		transparent: false,
-		hex: [ '#FF0000', '#000000', '#FFFFFFF' ]
+		solidColor: '#4455DD',
+		randColor: [ '#FF0000', '#000000', '#FFFFFFF' ],
+		radius: 20,
+
+		noiseType: 'gradient',
+		vertColumn: '#000000',
+		horizColmn: '#FFFFFF',
+
+		grid: false,
+		gridColor: "#FAC22A",
+		smallStep: 10,
+		largeStep: 100
+		
 	};
 
 	// function invoked when the form is submitted
@@ -16,4 +29,32 @@ app.controller('VelociraptorForm', ['$scope', function($scope)
 	{
 		console.log( this.model );
 	};
+
+		//console.log( "VelociraptorForm is active!!" );
 }]);
+	app.directive("canvasField", function ()
+  	{
+	    return {
+	        restrict: 'E',
+	        scope: {
+	            progress: '=',
+	            progressId: '@'
+	        },
+	        template: "<canvas id='pgcanvas' width='400' height='400'>",
+	        link: function(scope, element, attrs) {
+	           console.log(element);
+	           scope.canvas = element.find('canvas')[0];
+	           scope.context = scope.canvas.getContext('2d');
+	 
+	           scope.$watch('progress', function(newValue) {
+	             barWidth = Math.ceil(newValue / 100 * scope.canvas.width);
+	             scope.context.fillStyle = "#DDD";
+	             scope.context.fillRect(0, 0, scope.canvas.width, scope.canvas.height);
+	             scope.context.fillStyle = "#F00";
+	             scope.context.fillRect(0, 0, barWidth, scope.canvas.height);
+	           });
+	        }        
+		};
+	});
+
+
