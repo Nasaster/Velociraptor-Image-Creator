057077314d9301fa30f17c9febb2a32033ccdbf7
thanasis tsokaktsidis <nas.tsokaktsidis@gmail.com>
2015-07-17 17:01:17 +0200

    Develop pattern properties

--- source/js/controllers/PreviewController.js
+++ source/js/controllers/PreviewController.js
@@ -1,132 +1,231 @@
-// app.controller('VelociraptorPreview', ['$scope', function($scope)
-// {
+// is subscribed to data modified by the FormController
 
-// }]);
-
-// app.controller('VelociraptorPreview', function($scope) {
-//     $scope.name = 'World';
-// });
-
-app.directive('circleDraw', ['$timeout',
-    function(timer) {
-        return {
-            restrict: 'A',
-            link: function(scope, iElement) {
-                var context = iElement[0].getContext('2d');
-                var x = iElement[0].width / 2;
-                var y = iElement[0].height / 2;
-                var radius = 90;
-                var startAngle = 0 * Math.PI;
-                var endAngle;
-                var counterClockwise = false;
-                var strokeColor;
-
-                function getColor(huh) {
-                    if (huh <= 49) {
-                        strokeColor = '#d31f1f';
-                    } else if (huh <= 74) {
-                        strokeColor = '#eac11e';
-                    } else strokeColor = '#59b73c';
-                    console.log(strokeColor);
-                    return strokeColor;
-                }
-
-                //this test for rectangle works as expected 
-
-                //context.fillStyle = "#FF0000";
-                //context.fillRect(20, 20, 150, 150);
-
-                var getDeg = function(inPerc) {
-                    inPerc = 90;
-                    var aPerc = (inPerc * 0.02);
-                    console.log('aPerc = ' + aPerc);
-                    endAngle = aPerc * Math.PI;
-                    console.log("end angle = " + endAngle);
-                    console.log("start angle = " + startAngle);
-                    context.beginPath();
-                    context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
-                    context.lineWidth = 10;
-                    context.strokeStyle = getColor(inPerc);
-                    context.stroke();
-                }
-                timer(getDeg, 0);
-            }
-        }
-    }
-]);
-
-app.controller('VelociraptorPreview', function($scope, squareDrawer) 
+app.controller('VelociraptorPreview', function($scope, renderer) 
 {
-	$scope.classes = [
-        //"bg-buildings",
-        "red",
-        "blue",
-        "yellow",
-        "green",
-        "orange",
-        "black",
-        "purple"
-    ];        
+	var canvas = document.getElementById( "canvas" ),
+	    ctx    = canvas.getContext( "2d" );
+     
+	// invoked whenever the FormController broadcasts a form submit
+	// over the $rootScope
 
-     $scope.square = 
+	$scope.$on( 'update', function( event, eventData )
     {
-        height: '100',
-        width: '100'
-    };
+	 	var data = sanitizeForm( eventData ); // sanitize form dimensions from inches to pixels
 
-    $scope.$watch('square', function(newValue, oldValue) 
-    {
-        if (newValue != oldValue) 
-        {
-            console.log('value changed: ' + JSON.stringify($scope.square));
-            squareDrawer.draw(newValue.height, newValue.width);
-        }
-    }, true);
+	 	console.log( data ); // dump data in console, is model from FormController
 
+	 	// first scale the canvas to the requested dimensions
+
+	 	canvas.width  = data.width;
+    	canvas.height = data.height;
+
+    	//clear the canvas
+        ctx.clearRect( 0, 0, canvas.width, canvas.height );	
+
+	 	// which fill type render do we require ?
+
+	 	switch ( data.fillType )
+	 	{
+	 		case "solid":
+	 			renderer.drawSquare( ctx, 0, 0, data.width, data.height, data.solidColor );
+	 			break;
+
+ 			case "random":
+				renderer.drawSquare( ctx, 0, 0, data.width, data.height, "#" + Math.floor( Math.random() * 0xFFFFFF ).toString( 16 ));
+ 				break;
+
+			case "noise":
+				renderer.createNoise( ctx, data.width, data.height, 1 );
+				break;
+
+	 	}
+
+	 	if ( data.checkered ) {
+
+	 		renderer.createCheckeredPattern( ctx, data.width, data.height, data.rowAmount, data.columnAmount, data.solidColor );
+	 	}
+
+	 	if ( data.grid ) {
+
+	 	 	renderer.createGridGenerator( ctx, data.width, data.height, data.smallStep, data.largeStep, data.gridColor );
+	 	}
+
+	 	//
+	 	//renderer.drawCircle( data.radius, data.solidColor, data.randColor );
+	 	//renderer.triangle  ( data.height, data.width, 	   data.solidColor, data.randColor );
+	});
+
+	// cache the DPI for this particular device / screen
+	// this is the amount of pixels that fits in an inch
+	var ppi = document.querySelector( ".inch" ).offsetWidth;
+	
+	console.log( "pixels per inch: " + ppi );
+
+	// helper function to convert dimensions in inches to pixels
+
+	function inchesToPixels( inches ) {
+		return inches * ppi;
+	}
+
+	// helper function to sanitize form values into pixels
+
+	function sanitizeForm( form )
+	{
+		// we clone the form data as to not update the form view
+		var data = JSON.parse( JSON.stringify( form ));
+
+		// if unit is specified to be in inches, convert all
+		// associated dimensions into pixels
+
+		if ( data.unit === "in" )
+		{
+			data.width     = inchesToPixels( data.width );
+			data.height    = inchesToPixels( data.height );
+			data.radius    = inchesToPixels( data.radius );
+			data.smallStep = inchesToPixels( data.smallStep );
+			data.largeStep = inchesToPixels( data.largeStep );
+		}
+		return data;
+	}
+
+	// function exportCanvas( canvas, ctx)
+ //    {
+ //        if( canvas &amp;&amp; ctx ) 
+ //        {
+ //            var img = canvas.toDataURL("image/png;base64;");
+ //            anchor = document.getElementById("download");
+ //            anchor.href = img;
+ //            anchor.innerHTML = "Download";
+	//     }
+	//     else 
+	//     {
+ //             alert("Can not export");
+ //        }
+	// }
 });
 
-app.factory('squareDrawer', function()
+app.factory('renderer', function()
 {
-    var registry = {};
-
-    var squareDrawer = 
+    var renderer =
     {
+    	createNoise : function( ctx, width, height, opacity )
+    	{
+		    var x, y, color;
+		    var opacity = opacity || .2;
+		    var red, green, blue;
+		  
+		    for ( x = 0; x < width; x++ )
+		    {
+		       for ( y = 0; y < height; y++ )
+		       {
+		          red   = Math.floor( Math.random() * 255 );
+		  	      green = Math.floor( Math.random() * 255 );
+		          blue  = Math.floor( Math.random() * 255 );
+		  
+		          ctx.fillStyle = "rgba(" + red + "," + green + "," + blue + "," + opacity + ")";
+		          ctx.fillRect(x, y, 1, 1);
+		       }
+		    }
+    	},
+
+    	createCheckeredPattern( ctx, canvasWidth, canvasHeight, amountOfRows, amountOfColumns, blockColor )
+    	{
+    		var blockWidth  = canvasWidth  / amountOfColumns;
+    		var blockHeight = canvasHeight / amountOfRows;
+
+    		// render blocks in individual rows
+
+    		var rowFilled = true;	// whether the previous row started with a filled block
+    		var fill = true;
+
+    		for ( var row = 0; row < amountOfRows; ++row )
+    		{
+    			// render all columns within a row
+    			for ( var column = 0; column < amountOfColumns; ++column )
+    			{
+    				if ( fill ) {
+	    				renderer.drawSquare( ctx, column * blockWidth, row * blockHeight, blockWidth, blockHeight, blockColor );
+    				}
+    				fill = !fill;
+    			}
+    			rowFilled = !rowFilled;
+    			fill      = rowFilled;	// ensure first block of the next row matches the fill expectation of the row
+    		}
+    	},
+
+        createGridGenerator( ctx, canvasWidth, canvasHeight, smallStep, largeStep, gridColor, gridPixelSize )
+	    {
+
+		 /*
+		  * i is used for both x and y to draw
+	 	  * a line every 5 pixels starting at
+	 	  * .5 to offset the canvas edges
+	 	  */
+
+			for(var i = .5; i < canvasWidth || i < canvasHeight; i += 20) {
+            // draw horizontal lines
+            ctx.moveTo( i, 0 );
+            ctx.lineTo( i, canvasHeight);
+            // draw vertical lines
+            ctx.moveTo( 0, i );
+            ctx.lineTo( canvasWidth, i);
+        	}
+        
+        
+
+			ctx.strokeStyle = gridColor;
+			ctx.stroke();
+
+	    },
+
         /**
          * draw the square on the canvas
          */
-        draw: function(height, width) 
+        drawSquare : function( ctx, x, y, width, height, color ) 
         {
-            console.log("square to draw: " + height + "x" + width);
+            console.log("square to draw: " + width + "x" + height + " at " + x + " x " + y );
+            
+            ctx.fillStyle = color;
+            ctx.fillRect( x, y, width, height);
+        },
 
-            var canvas = document.getElementById("canvas");
-            if (canvas.getContext) 
-            {
-                console.log("drawing");
-                var ctx = canvas.getContext("2d");
-                //clear the canvas
-                ctx.clearRect(0,0, canvas.width, canvas.height);
+        triangle: function( ctx, width, height, color, randomColor)
+        {
+			ctx.moveTo(120, 120);
+			ctx.lineTo(120, 200);
+			ctx.lineTo(200, 200);
+			ctx.closePath();
+			 
+			// the fill color
+			ctx.fillStyle = color || randomColor;;
+			ctx.fill();
+        },
 
-                ctx.fillRect(0, 0, width, height);
+        drawCircle : function( ctx, radius, color, randomColor )
+        {
+            var x = canvas.width / 2;
+            var y = canvas.height / 2;
+            var startAngle = 0 * Math.PI;
+            var endAngle;
+            var counterClockwise = false;
+            
+
+            var getDeg = function(inPerc) {
+                var aPerc = (inPerc * 0.02);
+                console.log('aPerc = ' + aPerc);
+                endAngle = aPerc * Math.PI;
+                console.log("end angle = " + endAngle);
+                console.log("start angle = " + startAngle);
+                ctx.beginPath();
+                ctx.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+                ctx.lineWidth = 10;
+                ctx.strokeStyle = color || randomColor;
+                ctx.stroke();
+                ctx.fill();
             }
+            getDeg( 100 );
         }
     };
-
-
-    return squareDrawer;
+    return renderer;
 });
 
-app.directive("ngRandomClass", function () {
-    return {
-        restrict: 'EA',
-        replace: false,
-        scope: {
-            ngClasses: "="
-        },
-        // template: "<canvas id='pgcanvas' width='400' height='400'>",
-        link: function (scope, elem, attr) {            
-
-            //Add random background class to selected element
-            elem.addClass(scope.ngClasses[Math.floor(Math.random() * (scope.ngClasses.length))]);
-        }
-    }
-});
--- source/js/controllers/VelociraptorFormController.js
+++ source/js/controllers/VelociraptorFormController.js
@@ -1,61 +1,50 @@
-app.controller('VelociraptorForm', ['$scope', function($scope)
+app.controller('VelociraptorForm', [ '$rootScope', '$scope', function( $rootScope, $scope)
 {
 	// the model specifiying what the data looks like
 
 	this.model = {
-		width: 10,
-		height: 10,
+		width: 100,
+		height: 100,
 		unit: 'px',
 
 		transparent: false,
-		solidColor: '#4455DD',
-		randColor: [ '#FF0000', '#000000', '#FFFFFFF' ],
+		solidColor: '',
+		randColor: getRandomColor(),
 		radius: 20,
 
+		rowAmount: 5,
+		columnAmount: 5,
+
 		noiseType: 'gradient',
-		vertColumn: '#000000',
-		horizColmn: '#FFFFFF',
+		checkered: false,
+		vertColumn: '',
+		horizColmn: '',
 
 		grid: false,
-		gridColor: "#FAC22A",
-		smallStep: 10,
-		largeStep: 100
+		gridColor: '',
+		smallStep: 5,
+		largeStep: 10
 		
+	};
+	// Random Color function 
+	function getRandomColor() 
+	{
+    	var letters = '0123456789ABCDEF'.split('');
+    	var color = '#';
+    		for (var i = 0; i < 6; i++ ) 
+    		{
+        		color += letters[Math.floor(Math.random() * 16)];
+    		}
+    		return color;
 	};
 
 	// function invoked when the form is submitted
 
 	this.submit = function( form )
 	{
-		console.log( this.model );
+		// broadcast to all subscribers that the form has been submitted
+		$rootScope.$broadcast( 'update', this.model );
 	};
 
 		//console.log( "VelociraptorForm is active!!" );
 }]);
-
-
-
-app.directive("canvasField", function ()
-	{
-    return {
-        restrict: 'E',
-        scope: {
-            progress: '=', 		//set up progress to accept data-binding
-            progressId: '@'		// the progressId uses the data-binding from the parent scope
-        },
-        template: "<canvas id='pgcanvas' width='400' height='400'>",
-        link: function(scope, element, attrs) {
-           console.log(element);
-           scope.canvas = element.find('canvas')[0];
-           scope.context = scope.canvas.getContext('2d');
- 
-           scope.$watch('progress', function(newValue) {
-             barWidth = Math.ceil(newValue / 100 * scope.canvas.width);
-             scope.context.fillStyle = "#DDD";
-             scope.context.fillRect(0, 0, scope.canvas.width, scope.canvas.height);
-             scope.context.fillStyle = "#F00";
-             scope.context.fillRect(0, 0, barWidth, scope.canvas.height);
-           });
-        }        
-	};
-});
